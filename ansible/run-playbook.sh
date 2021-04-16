67298746a0ba32620c6a55a8ef8e4a8e9f3f98c9
Sylvain Baubeau <lebauce@gmail.com>
2021-04-16 09:33:13 +0200

    Modify run-playbook.sh to work for both master and slaves

--- ansible/run-playbook.sh
+++ ansible/run-playbook.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-
-# install only : jenkins slaves
 dir="$(dirname "$0")"
-ANSIBLE_ROLES_PATH="~/.ansible/roles:/usr/share/ansible/roles:/etc/ansible/roles:$dir/../../../contrib/ansible/roles" ansible-playbook -i inventory.yml -t slave --skip-tags "vagrant,jenkins_slave_setup" -e ansible_python_interpreter=/usr/bin/python3 -e jenkins_public_hostname=ci.skydive.network -e jenkins_admin_password=password deploy.yml
 
-# deploying master
-# -t master -e deploy_master=true -e jenkins_public_ip=1.2.3.4
+ansible-galaxy install -r requirements.yml
+
+export ANSIBLE_ROLES_PATH="~/.ansible/roles:/usr/share/ansible/roles:/etc/ansible/roles:$dir/../../skydive/contrib/ansible/roles"
+
+if [ "$1" = "master" ]
+then
+	ansible-playbook -i inventory.yml -t master -e deploy_master=true -e jenkins_public_ip=51.83.46.175 --skip-tags "vagrant,jenkins_slave_setup" -e ansible_python_interpreter=/usr/bin/python3 -e jenkins_public_hostname=ci.graffiti.community -e jenkins_admin_password=password deploy.yml
+else if [ "$1" = "slave" ]
+	ansible-playbook -i inventory.yml -t slave --skip-tags "vagrant,jenkins_slave_setup" -e ansible_python_interpreter=/usr/bin/python3 -e jenkins_public_hostname=ci.graffiti.community -e jenkins_admin_password=password deploy.yml
+fi
